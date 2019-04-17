30ea2925117585c7eb1d2f0fb554b2f351105eea
Administrator <867342143@qq.com>
2019-04-18 00:32:06 +0800

    前端登录界面

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,24 +10,12 @@ class Index extends Frontend
 
     protected $noNeedLogin = '*';
     protected $noNeedRight = '*';
-    protected $layout = '';
+    protected $layout = 'default';
 
     public function index()
     {
-        $socket = socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
-        $result = socket_connect($socket,'46.29.162.104',21560);
-        if ($result < 0) {
-            echo "socket_connect() failed.\nReason: ($result) " . socket_strerror($result) . "\n";
-        }else{
-            echo "连接OK\n";
-        }
 
         return $this->view->fetch();
-    }
-
-    public function news()
-    {
-
     }
 
 }
