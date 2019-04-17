1535de95dc8c19bd70594cf10167b3de612804c0
xuzhuangzhuang <867342143@qq.com>
2019-04-17 11:20:17 +0800

    ssr订阅采集

---
+++ runtime/temp/91322272becb8b04bb6398c7d1a5f3d5.php
@@ -0,0 +1,162 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:4:{s:73:"E:\Project\ssrshare\public/../application/admin\view\ssr\config\edit.html";i:1555397313;s:62:"E:\Project\ssrshare\application\admin\view\layout\default.html";i:1554882264;s:59:"E:\Project\ssrshare\application\admin\view\common\meta.html";i:1554882264;s:61:"E:\Project\ssrshare\application\admin\view\common\script.html";i:1554882264;}*/ ?>
+<!DOCTYPE html>
+<html lang="<?php echo $config['language']; ?>">
+    <head>
+        <meta charset="utf-8">
+<title><?php echo (isset($title) && ($title !== '')?$title:''); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="renderer" content="webkit">
+
+<link rel="shortcut icon" href="/assets/img/favicon.ico" />
+<!-- Loading Bootstrap -->
+<link href="/assets/css/backend<?php echo \think\Config::get('app_debug')?'':'.min'; ?>.css?v=<?php echo \think\Config::get('site.version'); ?>" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+<!--[if lt IE 9]>
+  <script src="/assets/js/html5shiv.js"></script>
+  <script src="/assets/js/respond.min.js"></script>
+<![endif]-->
+<script type="text/javascript">
+    var require = {
+        config:  <?php echo json_encode($config); ?>
+    };
+</script>
+    </head>
+
+    <body class="inside-header inside-aside <?php echo defined('IS_DIALOG') && IS_DIALOG ? 'is-dialog' : ''; ?>">
+        <div id="main" role="main">
+            <div class="tab-content tab-addtabs">
+                <div id="content">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <section class="content-header hide">
+                                <h1>
+                                    <?php echo __('Dashboard'); ?>
+                                    <small><?php echo __('Control panel'); ?></small>
+                                </h1>
+                            </section>
+                            <?php if(!IS_DIALOG && !$config['fastadmin']['multiplenav']): ?>
+                            <!-- RIBBON -->
+                            <div id="ribbon">
+                                <ol class="breadcrumb pull-left">
+                                    <li><a href="dashboard" class="addtabsit"><i class="fa fa-dashboard"></i> <?php echo __('Dashboard'); ?></a></li>
+                                </ol>
+                                <ol class="breadcrumb pull-right">
+                                    <?php foreach($breadcrumb as $vo): ?>
+                                    <li><a href="javascript:;" data-url="<?php echo $vo['url']; ?>"><?php echo $vo['title']; ?></a></li>
+                                    <?php endforeach; ?>
+                                </ol>
+                            </div>
+                            <!-- END RIBBON -->
+                            <?php endif; ?>
+                            <div class="content">
+                                <form id="edit-form" class="form-horizontal" role="form" data-toggle="validator" method="POST" action="">
+
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Share_id'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-share_id" data-rule="required" data-source="share/index" class="form-control selectpage" name="row[share_id]" type="text" value="<?php echo $row['share_id']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Address'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-address" data-rule="required" class="form-control" name="row[address]" type="text" value="<?php echo $row['address']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Port'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-port" data-rule="required" class="form-control" name="row[port]" type="number" value="<?php echo $row['port']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Password'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-password" data-rule="required" class="form-control" name="row[password]" type="text" value="<?php echo $row['password']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Method'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-method" data-rule="required" class="form-control" name="row[method]" type="text" value="<?php echo $row['method']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Protocol'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-protocol" data-rule="required" class="form-control" name="row[protocol]" type="text" value="<?php echo $row['protocol']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Protocol_param'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-protocol_param" data-rule="required" class="form-control" name="row[protocol_param]" type="text" value="<?php echo $row['protocol_param']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Obfs'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-obfs" data-rule="required" class="form-control" name="row[obfs]" type="text" value="<?php echo $row['obfs']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Obfs_param'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-obfs_param" data-rule="required" class="form-control" name="row[obfs_param]" type="text" value="<?php echo $row['obfs_param']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Group'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-group" data-rule="required" class="form-control" name="row[group]" type="text" value="<?php echo $row['group']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Remark'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-remark" data-rule="required" class="form-control" name="row[remark]" type="text" value="<?php echo $row['remark']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Ssrurl'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-ssrurl" data-rule="required" class="form-control" name="row[ssrurl]" type="text" value="<?php echo $row['ssrurl']; ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Status'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            
+            <div class="radio">
+            <?php if(is_array($statusList) || $statusList instanceof \think\Collection || $statusList instanceof \think\Paginator): if( count($statusList)==0 ) : echo "" ;else: foreach($statusList as $key=>$vo): ?>
+            <label for="row[status]-<?php echo $key; ?>"><input id="row[status]-<?php echo $key; ?>" name="row[status]" type="radio" value="<?php echo $key; ?>" <?php if(in_array(($key), is_array($row['status'])?$row['status']:explode(',',$row['status']))): ?>checked<?php endif; ?> /> <?php echo $vo; ?></label> 
+            <?php endforeach; endif; else: echo "" ;endif; ?>
+            </div>
+
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-12 col-sm-2"><?php echo __('Timeout'); ?>:</label>
+        <div class="col-xs-12 col-sm-8">
+            <input id="c-timeout" data-rule="required" class="form-control" name="row[timeout]" type="number" value="<?php echo $row['timeout']; ?>">
+        </div>
+    </div>
+    <div class="form-group layer-footer">
+        <label class="control-label col-xs-12 col-sm-2"></label>
+        <div class="col-xs-12 col-sm-8">
+            <button type="submit" class="btn btn-success btn-embossed disabled"><?php echo __('OK'); ?></button>
+            <button type="reset" class="btn btn-default btn-embossed"><?php echo __('Reset'); ?></button>
+        </div>
+    </div>
+</form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="/assets/js/require<?php echo \think\Config::get('app_debug')?'':'.min'; ?>.js" data-main="/assets/js/require-backend<?php echo \think\Config::get('app_debug')?'':'.min'; ?>.js?v=<?php echo $site['version']; ?>"></script>
+    </body>
+</html>
---
+++ runtime/temp/c234d337082fdf947b3ae0fbb23a4124.php
@@ -0,0 +1,1 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:73:"E:\Project\php\ssrshare\public/../application/index\view\index\index.html";i:1555386944;}*/ ?>
