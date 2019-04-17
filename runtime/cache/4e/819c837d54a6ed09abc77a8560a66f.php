dfab4bcbc3dac52b02c96f84385db37053c7083e
xuzhuangzhuang <867342143@qq.com>
2019-04-17 13:49:21 +0800

    删除不需要的文件夹

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{s:8:"testhook";a:1:{i:0;s:25:"\addons\markdown\Markdown";}}
--- runtime/temp/af205de39dccbec2ef4e7d08653692cf.php
+++
@@ -1,1 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:69:"E:\Project\ssrshare\public/../application/index\view\index\index.html";i:1555386944;}*/ ?>
