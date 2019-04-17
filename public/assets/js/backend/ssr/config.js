072ee5af96b23f004b2a44f9fbbfc64d40edaff8
xuzhuangzhuang <867342143@qq.com>
2019-04-17 10:27:59 +0800

    ssr订阅采集

---
+++ public/assets/js/backend/ssr/config.js
@@ -0,0 +1,65 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'ssr/config/index' + location.search,
+                    add_url: 'ssr/config/add',
+                    edit_url: 'ssr/config/edit',
+                    del_url: 'ssr/config/del',
+                    multi_url: 'ssr/config/multi',
+                    table: 'ssr_config',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id'),visible:false},
+                        {field: 'share.name', title: __('Share.name')},
+                        {field: 'address', title: __('Address')},
+                        {field: 'port', title: __('Port')},
+                        {field: 'password', title: __('Password')},
+                        {field: 'method', title: __('Method')},
+                        {field: 'protocol', title: __('Protocol')},
+                        {field: 'protocol_param', title: __('Protocol_param'),visible:false},
+                        {field: 'obfs', title: __('Obfs')},
+                        {field: 'obfs_param', title: __('Obfs_param'),visible:false},
+                        {field: 'group', title: __('Group'),visible:false},
+                        {field: 'remark', title: __('Remark'),visible:false},
+                        {field: 'ssrurl', title: __('Ssrurl'), formatter: Table.api.formatter.url,visible:false},
+                        {field: 'status', title: __('Status'), searchList: {"0":__('Status 0'),"-1":__('Status -1'),"1":__('Status 1')}, formatter: Table.api.formatter.status},
+                        {field: 'timeout', title: __('Timeout')},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime,visible:false},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime,visible:false},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
---
+++ public/assets/js/backend/ssr/share.js
@@ -0,0 +1,56 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'ssr/share/index' + location.search,
+                    add_url: 'ssr/share/add',
+                    edit_url: 'ssr/share/edit',
+                    del_url: 'ssr/share/del',
+                    multi_url: 'ssr/share/multi',
+                    table: 'share',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name')},
+                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                        {field: 'marker_content', title: __('Marker_content')},
+                        {field: 'url_status', title: __('Url_status'),searchList: {"0":__('Url_status 0'),"1":__('Url_status 1')}, formatter: Table.api.formatter.status},
+                        {field: 'status', title: __('Status'), searchList: {"0":__('Status 0'),"1":__('Status 1')}, formatter: Table.api.formatter.status},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
