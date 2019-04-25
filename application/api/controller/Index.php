9f2777bf35dde79520f6a19754f7c65fcafcc4ab
xuzhuangzhuang <867342143@qq.com>
2019-04-25 17:06:24 +0800

    专用接口

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -2,6 +2,7 @@
 
 namespace app\api\controller;
 
+use app\admin\model\ssr\Config as ConfigModel;
 use app\common\controller\Api;
 
 /**
@@ -19,5 +20,17 @@ class Index extends Api
     public function index()
     {
         $this->success('请求成功');
+    }
+    public function ssr()
+    {
+        $configModel = new ConfigModel();
+        $ssrList = $configModel->where(['status'=>1])->order('timeout asc')->limit(20)->select();
+        $ssr_str='';
+        foreach ($ssrList as $k => $v)
+        {
+            $config_str = $v['address'].':'.$v['port'].':'.$v['protocol'].':'.$v['method'].':'.$v['obfs'].':'.base64_encode($v['password']).'/?obfsparam='.base64_encode($v['obfs_param']).'&protoparam='.base64_encode($v['protocol_param']).'&remarks='.base64_encode('低延迟').'&group='.base64_encode('专用SSR');
+            $ssr_str.='ssr://'.base64_encode($config_str)."\n\r";
+        }
+        return base64_encode($ssr_str);
     }
 }
