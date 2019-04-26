f71ab3cebb424bb5f0197ad32a65051ef15655aa
xuzhuangzhuang <867342143@qq.com>
2019-04-26 21:08:00 +0800

    解析方式

--- application/admin/model/SsrConfig.php
+++ application/admin/model/SsrConfig.php
@@ -8,5 +8,30 @@ class SsrConfig extends Model
 {
     // 表名
     protected $name = 'ssr_config';
+
+    public static function analyze($analyze_id,$content)
+    {
+        switch ($analyze_id)
+        {
+            case 1:
+                return self::analyzeCommon($content);
+                break;
+            case 2:
+                self::analyzeASE($content);
+                break;
+        }
+    }
+
+    private function analyzeCommon($content)
+    {
+        $content = base64_decode($content);
+        $content = explode("\n", $content);
+        return $content;
+    }
+
+    private function analyzeASE($content)
+    {
+
+    }
     
 }
