33042251d0bf0d832cd731bbef12deb3601125d1
xuzhuangzhuang <867342143@qq.com>
2019-04-28 11:46:31 +0800

    增加 验证

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -23,14 +23,19 @@ class Index extends Api
     }
     public function ssr()
     {
-        $configModel = new ConfigModel();
-        $ssrList = $configModel->where(['status'=>1])->order('timeout asc')->limit(50)->select();
-        $ssr_str='';
-        foreach ($ssrList as $k => $v)
-        {
-            $config_str = $v['address'].':'.$v['port'].':'.$v['protocol'].':'.$v['method'].':'.$v['obfs'].':'.base64_encode($v['password']).'/?obfsparam='.base64_encode($v['obfs_param']).'&protoparam='.base64_encode($v['protocol_param']).'&remarks='.base64_encode('低延迟').'&group='.base64_encode('专用SSR');
-            $ssr_str.='ssr://'.base64_encode($config_str)."\n\r";
+        $token = $this->request->header('token');
+        if ($token){
+            trace(md5($token),'zhanzheng');
+            $configModel = new ConfigModel();
+            $ssrList = $configModel->where(['status'=>1])->order('timeout asc')->limit(50)->select();
+            $ssr_str='';
+            foreach ($ssrList as $k => $v)
+            {
+                $config_str = $v['address'].':'.$v['port'].':'.$v['protocol'].':'.$v['method'].':'.$v['obfs'].':'.base64_encode($v['password']).'/?obfsparam='.base64_encode($v['obfs_param']).'&protoparam='.base64_encode($v['protocol_param']).'&remarks='.base64_encode('低延迟').'&group='.base64_encode('专用SSR');
+                $ssr_str.='ssr://'.base64_encode($config_str)."\n\r";
+            }
+            return base64_encode($ssr_str);
         }
-        return base64_encode($ssr_str);
+        echo '<script src="'. $this->request->root().'/assets/js/verification.js"></script>';
     }
 }
