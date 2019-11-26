cf8d0fa3200def98031ebdafa656f8c241b02257
xuzhuangzhuang <867342143@qq.com>
2019-11-26 17:18:31 +0800

    采集详细信息

---
+++ application/common/model/Baidu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+/**
+ * 百度采集详情库
+ */
+class Baidu Extends Model
+{
+
+}
---
+++ application/common/model/BaiduPwd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+/**
+ * 百度采集密码库
+ */
+class BaiduPwd Extends Model
+{
+
+}
